ae28e6e52fb14b1e5164d1d746d19a36f9b741c4
Antonio Alexan Brito <antonioalexanbrito@Antonios-MacBook-Pro.local>
2020-08-27 13:42:45 -0400

    Created the sidebar component

---
+++ src/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Sidebar.css'
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      {/* SIdebar Icons */}
+      
+      {/* SIdebar option */}
+      {/* SIdebar option */}
+      {/* SIdebar option */}
+      {/* SIdebar option */}
+      {/* SIdebar option */}
+      {/* SIdebar option */}
+
+      {/* Button -> What I learn today */}
+    </div>
+  )
+}
+
+export default Sidebar
+
