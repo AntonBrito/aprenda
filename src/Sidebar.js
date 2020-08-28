a2eda3b86139d02522dd1edc572b4f370b4fbef0
Antonio Alexan Brito <antonioalexanbrito@Antonios-MacBook-Pro.local>
2020-08-28 13:29:00 -0400

    Imported some of icons for sidebaroption

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import './Sidebar.css';
+import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 import HomeIcon from '@material-ui/icons/Home';
+import SidebarOption from './SidebarOption';
+import AddIcon from '@material-ui/icons/Add';
+import PageviewIcon from '@material-ui/icons/Pageview';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import PersonIcon from '@material-ui/icons/Person';
+import FolderSharedIcon from '@material-ui/icons/FolderShared';
+import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder'
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <HomeIcon />
+      <WbIncandescentIcon />
+      
+      <SidebarOption Icon={HomeIcon} text="Home"/>
+      <SidebarOption Icon={PageviewIcon} text="Explore"/>
+      <SidebarOption Icon={NotificationsActiveIcon} text="Notifications"/>
+      <SidebarOption Icon={PersonIcon} text="My Profile"/>
+      <SidebarOption Icon={FolderSharedIcon} text="My Beans"/>
+      <SidebarOption Icon={BookmarkBorderIcon} text="My Bookmarks"/>
+      <SidebarOption Icon={AddIcon} text="New Beans"/>
 
-      {/* SIdebar option */}
-      {/* SIdebar option */}
-      {/* SIdebar option */}
-      {/* SIdebar option */}
-      {/* SIdebar option */}
-      {/* SIdebar option */}
-
-      {/* Button -> What I learn today */}
+      {/* Button -> today's Beans */}
     </div>
   )
 }
