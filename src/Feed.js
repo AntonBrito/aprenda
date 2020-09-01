87eb7ed974ca08c5479dd93a5fe34c09a2259bbb
AntonBrito <tonybrito85@gmail.com>
2020-09-01 13:04:35 -0400

    Created the feed component

---
+++ src/Feed.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Feed.css';
+
+function Feed() {
+  return (
+    <div>
+      {/*Header*/}
+      <h2>Home</h2>
+      {/*BeanBox*/}
+      {/*Post*/}
+      {/*Post*/}
+    </div>
+  )
+}
+
+export default Feed
+
+
