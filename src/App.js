f46c3d294ae643db11eb87c68f02fae342002035
AntonBrito <tonybrito85@gmail.com>
2020-09-01 22:15:20 -0400

    Created a widget container

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Sidebar from './Sidebar.js'
 import Feed from './Feed.js';
+import Widgets from "./Widgets.js"
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       {/* Feed */}
       <Feed />
       {/* Widgets */}
+      <Widgets />
     </div>
   );
 }
