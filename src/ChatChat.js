dd593a1b6df221f28162bce4bed093c77eed0948
AntonBrito <tonybrito85@gmail.com>
2020-09-20 19:19:10 -0400

    Deleted extra code, created chatchat file

---
+++ src/ChatChat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ChatChat extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ChatChat</h1>
+      </div>
+    )
+  }
+}
+
+export default ChatChat
