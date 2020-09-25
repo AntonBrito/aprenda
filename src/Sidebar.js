38f4b89a719611a8b0a57b43b31643540bf8fe04
AntonBrito <tonybrito85@gmail.com>
2020-09-25 13:37:46 -0400

    Cleaned extra code.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,15 +2,8 @@ import React from 'react'
 import './Sidebar.css';
 import HomeIcon from '@material-ui/icons/Home';
 import SidebarOption from './SidebarOption';
-import PageviewIcon from '@material-ui/icons/Pageview';
-import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
 import PersonIcon from '@material-ui/icons/Person';
 import FolderSharedIcon from '@material-ui/icons/FolderShared';
-import BookmarkBorderIcon from '@material-ui/icons/BookmarkBorder';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import { Button } from "@material-ui/core"
-
-
 
 function Sidebar() {
   return (
@@ -18,7 +11,6 @@ function Sidebar() {
       <SidebarOption Icon={HomeIcon} text="Home"/>
       <SidebarOption Icon={PersonIcon} text="My Profile"/>
       <SidebarOption Icon={FolderSharedIcon} text="My Beans"/>
-      <SidebarOption Icon={BookmarkBorderIcon} text="My Bookmarks"/>
     </div>
   )
 }
